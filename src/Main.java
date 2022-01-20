2ed1d9de7016b855b766fced37ff18a74298c10a
RichardSales <richardgti28@gmail.com>
2022-01-19 21:01:25 -0300

    Projeto de operacoes matematicas ii

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+	int valor1 = sc.nextInt();
+	int valor2 = sc.nextInt();
+	int soma = valor1 + valor2;	
+		
+	System.out.printf("SOMA = %d", soma);
+	
+	sc.close();
+	
+	}
+
+}
